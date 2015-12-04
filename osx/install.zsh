547d7f8aa2794a66283b6cc370bdf8e9de1de9fd
xiaoxing <dawnstar.hu@gmail.com>
2015-12-04 16:08:38 +1300

    add dict.

--- osx/install.zsh
+++ osx/install.zsh
@@ -2,10 +2,17 @@
 
 setopt EXTENDED_GLOB
 
-for file in "$(pwd -P)"/^install.zsh(.N); do
+for file in "$(pwd -P)"/LaunchAgents/*.plist; do
   target="${ZDOTDIR:-$HOME}/Library/LaunchAgents/${file:t}"
   if [[ -h "$target" || -a "$target" ]]; then
     rm "$target"
   fi
   ln -s "$file" "$target"
 done
+
+dict="$(pwd -P)"/spelling/LocalDictionary
+target="${ZDOTDIR:-$HOME}/Library/Spelling/${dict:t}"
+if [[ -h "$target" || -a "$target" ]]; then
+  rm "$target"
+fi
+ln -s "$dict" "$target"
