3d7375fbb1548a3bb9e63a074b1d750df2b1f254
Xiaoxing Hu <dawnstar.hu@gmail.com>
2016-12-13 23:06:35 +1300

    tweak emacs config

--- emacs/install.zsh
+++ emacs/install.zsh
@@ -6,12 +6,13 @@ if [[ ! -d ~/.emacs.d ]]; then
   git clone --recursive https://github.com/syl20bnr/spacemacs ~/.emacs.d
 fi
 
-for rcfile in "$(pwd -P)"/^install.zsh(.N); do
-  target="${ZDOTDIR:-$HOME}/.${rcfile:t}"
+dotfiles=(spacemacs.d)
+for dotfile in $dotfiles; do
+  target="${ZDOTDIR:-$HOME}/.$dotfile"
   if [[ -h "$target" || -a "$target" ]]; then
     rm "$target"
   fi
-  ln -s "$rcfile" "$target"
+  ln -s "$(pwd -P)"/$dotfile "$target"
 done
 
 for layer in "$(pwd -P)"/layers/*; do
