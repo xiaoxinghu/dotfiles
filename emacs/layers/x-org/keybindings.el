d9bfa843a4b1202d7b38e8366397ae538a844092
Xiaoxing Hu <dawnstar.hu@gmail.com>
2017-02-28 12:23:12 +1300

    cleanup

--- emacs/layers/x-org/keybindings.el
+++
@@ -1,6 +0,0 @@
-(evil-leader/set-key "ot" 'x-org/quick-template)
-
-(evil-leader/set-key "oc" 'org-capture)
-(evil-leader/set-key "oa" 'org-agenda)
-
-(global-set-key (kbd "s-k") 'x-org/insert-link)
