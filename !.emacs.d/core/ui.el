512e72ae3a6d4e4f0c85355e401515260f490b5b
Xiaoxing Hu <dawnstar.hu@gmail.com>
2019-01-31 23:46:06 +1300

    pretty org

--- !.emacs.d/core/ui.el
+++ !.emacs.d/core/ui.el
@@ -16,9 +16,15 @@
   ;; or for treemacs users
   (doom-themes-treemacs-config)
   ;; Enable flashing mode-line on errors
-  ;;(doom-themes-visual-bell-config)
+  (doom-themes-visual-bell-config)
   ;; Corrects (and improves) org-mode's native fontification.
   (doom-themes-org-config))
+
+;; (use-package poet-theme
+;;   :config
+;;   (add-hook 'text-mode-hook
+;; 	    (lambda ()
+;; 	      (variable-pitch-mode 1))))
 
 (use-package solaire-mode
   :hook ((change-major-mode after-revert ediff-prepare-buffer) . turn-on-solaire-mode)
--- !.emacs.d/modules/init-org.el
+++ !.emacs.d/modules/init-org.el
@@ -3,11 +3,11 @@
 (defun +org|setup-basic ()
   (setq-default
     org-log-into-drawer 1
-    org-adapt-indentation nil
+    org-adapt-indentation t
     org-log-done 'time
     org-ellipsis "  "
     org-pretty-entities t
-    org-hide-emphasis-markers nil
+    org-hide-emphasis-markers t
     org-archive-mark-done nil
     org-image-actual-width nil
     org-hide-leading-stars t
@@ -24,7 +24,54 @@
        ("LATER" :inherit warning :weight bold))
     org-refile-targets
     '((nil :maxlevel . 3)
-       (org-agenda-files :maxlevel . 3))))
+      (org-agenda-files :maxlevel . 3))))
+
+(defun +org|setup-ui ()
+  (font-lock-add-keywords 'org-mode
+			  '(("^ *\\([-]\\) "
+			     (0 (prog1 () (compose-region (match-beginning 1) (match-end 1) "•"))))))
+
+  (let* ((variable-tuple
+	  (cond ((x-list-fonts "Source Sans Pro") '(:font "Source Sans Pro"))
+		((x-list-fonts "Lucida Grande")   '(:font "Lucida Grande"))
+		((x-list-fonts "Verdana")         '(:font "Verdana"))
+		((x-family-fonts "Sans Serif")    '(:family "Sans Serif"))
+		(nil (warn "Cannot find a Sans Serif Font.  Install Source Sans Pro."))))
+	 (base-font-color     (face-foreground 'default nil 'default))
+	 (headline           `(:inherit default :weight bold :foreground ,base-font-color)))
+
+    (custom-theme-set-faces
+     'user
+     `(org-level-8 ((t (,@headline ,@variable-tuple))))
+     `(org-level-7 ((t (,@headline ,@variable-tuple))))
+     `(org-level-6 ((t (,@headline ,@variable-tuple))))
+     `(org-level-5 ((t (,@headline ,@variable-tuple))))
+     `(org-level-4 ((t (,@headline ,@variable-tuple :height 1.1))))
+     `(org-level-3 ((t (,@headline ,@variable-tuple :height 1.25))))
+     `(org-level-2 ((t (,@headline ,@variable-tuple :height 1.5))))
+     `(org-level-1 ((t (,@headline ,@variable-tuple :height 1.75))))
+     `(org-document-title ((t (,@headline ,@variable-tuple :height 2.0 :underline nil))))))
+
+  (custom-theme-set-faces
+   'user
+   '(variable-pitch ((t (:family "Source Sans Pro" :height 180 :weight light))))
+   '(fixed-pitch ((t ( :family "Inconsolata" :slant normal :weight normal :height 1.0 :width normal)))))
+
+  (add-hook 'org-mode-hook 'variable-pitch-mode)
+  (add-hook 'org-mode-hook 'visual-line-mode)
+
+  (custom-theme-set-faces
+   'user
+   '(org-block                 ((t (:inherit fixed-pitch))))
+   '(org-document-info         ((t (:foreground "dark orange"))))
+   '(org-document-info-keyword ((t (:inherit (shadow fixed-pitch)))))
+   '(org-link                  ((t (:foreground "royal blue" :underline t))))
+   '(org-meta-line             ((t (:inherit (font-lock-comment-face fixed-pitch)))))
+   '(org-property-value        ((t (:inherit fixed-pitch))) t)
+   '(org-special-keyword       ((t (:inherit (font-lock-comment-face fixed-pitch)))))
+   '(org-tag                   ((t (:inherit (shadow fixed-pitch) :weight bold :height 0.8))))
+   '(org-verbatim              ((t (:inherit (shadow fixed-pitch))))))
+  )
 
 (defun +org|setup-agenda ()
   (setq org-agenda-window-setup 'other-window
@@ -87,6 +134,7 @@
   ;; (add-hook 'org-mode-hook 'flyspell-mode)
   :config
   (+org|setup-basic)
+  (+org|setup-ui)
   (+org|setup-agenda)
   (+org|setup-capture)
   (+org|setup-babel)
@@ -133,6 +181,7 @@
     :filter-return #'+org-attach*download-fullname))
 
 (use-package org-bullets
+  :quelpa (org-bullets :fetcher github :repo "Kaligule/org-bullets")
   :hook (org-mode . org-bullets-mode))
 
 (use-package org-fancy-priorities
