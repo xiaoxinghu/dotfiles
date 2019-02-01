09ebd994eb4eaaa3ed914e38bb11639fceaf64ed
Xiaoxing Hu <dawnstar.hu@gmail.com>
2019-02-01 21:01:02 +1300

    flycheck-posframe

--- !.emacs.d/modules/init-flycheck.el
+++ !.emacs.d/modules/init-flycheck.el
@@ -6,6 +6,7 @@
   :config
   ;; Emacs feels snappier without checks on newline
   (setq flycheck-check-syntax-automatically '(save idle-change mode-enabled))
+  (global-flycheck-mode +1)
   (defhydra hydra-flycheck
     (:pre (progn (setq hydra-lv t) (flycheck-list-errors))
      :post (progn (setq hydra-lv nil) (quit-windows-on "*Flycheck errors*"))
@@ -18,5 +19,22 @@
     ("G"  (progn (goto-char (point-max)) (flycheck-previous-error)) "Last")
     ("q"  nil))
   )
+
+;; (use-package flycheck-popup-tip
+;;   :commands (flycheck-popup-tip-show-popup flycheck-popup-tip-delete-popup)
+;;   :init (add-hook 'flycheck-mode-hook 'flycheck-popup-tip-mode)
+;;   :config (setq flycheck-popup-tip-error-prefix "✕ "))
+
+(use-package flycheck-posframe
+  :ensure t
+  :after flycheck
+  :config (add-hook 'flycheck-mode-hook #'flycheck-posframe-mode))
+
+;; (use-package flycheck-posframe
+;;   :commands flycheck-posframe-show-posframe
+;;   :config
+;;   (setq flycheck-posframe-warning-prefix "⚠ "
+;;         flycheck-posframe-info-prefix "··· "
+;;         flycheck-posframe-error-prefix "✕ "))
 
 (provide 'init-flycheck)
