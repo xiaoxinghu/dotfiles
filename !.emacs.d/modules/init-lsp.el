288f7d4d99ff8566aa273141c02fe5682186bf50
Xiaoxing Hu <dawnstar.hu@gmail.com>
2019-03-04 16:40:41 +1300

    indentation

--- !.emacs.d/modules/init-lsp.el
+++ !.emacs.d/modules/init-lsp.el
@@ -5,13 +5,13 @@
   (add-hook 'lsp-after-open-hook 'lsp-enable-imenu)
   :init
   (setq lsp-eldoc-render-all nil
-	lsp-inhibit-message t
-	lsp-message-project-root-warning t
-	lsp-prefer-flymake nil
-	lsp-message-project-root-warning t
-	lsp-auto-guess-root t)
+    lsp-inhibit-message t
+    lsp-message-project-root-warning t
+    lsp-prefer-flymake nil
+    lsp-message-project-root-warning t
+    lsp-auto-guess-root t)
   (setq lsp-clients-typescript-server "typescript-language-server"
-	lsp-clients-typescript-server-args '("--stdio"))
+    lsp-clients-typescript-server-args '("--stdio"))
   )
 
 (use-package lsp-ui
