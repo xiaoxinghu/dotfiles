be6f8e6d97be6c05e636c9ca9dec6e7e6eb277c4
Xiaoxing Hu <dawnstar.hu@gmail.com>
2019-03-05 16:55:12 +1300

    tweak lsp

--- !.emacs.d/modules/init-lsp.el
+++ !.emacs.d/modules/init-lsp.el
@@ -1,22 +1,33 @@
 (use-package lsp-mode
   ;; :quelpa (lsp-mode :fetcher github :repo "emacs-lsp/lsp-mode")
+  :init
+  (setq
+    lsp-session-file (concat x/etc-dir "lsp-session")
+    lsp-auto-guess-root t
+    lsp-keep-workspace-alive nil
+    lsp-eldoc-render-all nil
+    lsp-inhibit-message t
+    lsp-message-project-root-warning t)
+  (add-hook 'kill-emacs-hook (setq lsp-restart 'ignore))
+
   :config
   (require 'lsp-clients)
   (add-hook 'lsp-after-open-hook 'lsp-enable-imenu)
-  :init
-  (setq lsp-eldoc-render-all nil
-    lsp-inhibit-message t
-    lsp-message-project-root-warning t
-    lsp-prefer-flymake nil
-    lsp-message-project-root-warning t
-    lsp-auto-guess-root t)
   )
 
 (use-package lsp-ui
-  :after (lsp))
+  :after (lsp)
+  :config
+  (setq
+    lsp-prefer-flymake nil
+    lsp-ui-doc-max-height 8
+    lsp-ui-doc-max-width 35
+    lsp-ui-sideline-ignore-duplicate t)
+  )
 
 (use-package company-lsp
   ;; :quelpa (company-lsp :fetcher github :repo "tigersoldier/company-lsp")
+  :after lsp-mode
   :config
   (push 'company-lsp company-backends)
   (setq company-lsp-async t))
