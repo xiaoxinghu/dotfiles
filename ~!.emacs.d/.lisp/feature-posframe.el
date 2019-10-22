5115178773223f6e2eac500a8dbf4532ce181a68
Xiaoxing Hu <dawnstar.hu@gmail.com>
2019-10-22 16:46:44 +1300

    fix posframe border

--- ~!.emacs.d/.lisp/feature-posframe.el
+++ ~!.emacs.d/.lisp/feature-posframe.el
@@ -2,7 +2,7 @@
   :config
   (setq posframe-arghandler #'hemacs-posframe-arghandler)
   (defun hemacs-posframe-arghandler (posframe-buffer arg-name value)
-    (let ((info '(:internal-border-width 12 :min-width 80)))
+    (let ((info '(:internal-border-width 2 :min-width 80)))
       (or (plist-get info arg-name) value))))
 
 (use-package ivy-posframe
@@ -13,6 +13,11 @@
    '((swiper . ivy-posframe-display-at-window-bottom-left)
      (ivy-switch-buffer . ivy-posframe-display-at-window-center)
      (t . ivy-posframe-display)))
+  (ivy-fixed-height-minibuffer nil)
+  (ivy-posframe-border-width 10)
+  (ivy-posframe-parameters
+   `((min-width . 80)
+     (min-height . ,ivy-height)))
   :config
   (ivy-posframe-mode))
 
