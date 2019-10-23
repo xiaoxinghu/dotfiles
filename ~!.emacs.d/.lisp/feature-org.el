970f34f3b20bb4bc40549dea1e60895e62b01f80
Xiaoxing Hu <xiaoxing@huxx.org>
2019-10-23 22:11:46 +1300

    fix org-mode style

--- ~!.emacs.d/.lisp/feature-org.el
+++ ~!.emacs.d/.lisp/feature-org.el
@@ -62,16 +62,16 @@
      `(org-level-7 ((t (,@headline ,@variable-tuple))))
      `(org-level-6 ((t (,@headline ,@variable-tuple))))
      `(org-level-5 ((t (,@headline ,@variable-tuple))))
-     `(org-level-4 ((t (,@headline ,@variable-tuple :height 1.1))))
-     `(org-level-3 ((t (,@headline ,@variable-tuple :height 1.25))))
-     `(org-level-2 ((t (,@headline ,@variable-tuple :height 1.5))))
-     `(org-level-1 ((t (,@headline ,@variable-tuple :height 1.75))))
-     `(org-document-title ((t (,@headline ,@variable-tuple :height 2.0 :underline nil))))))
+     `(org-level-4 ((t (,@headline ,@variable-tuple :height 1))))
+     `(org-level-3 ((t (,@headline ,@variable-tuple :height 1.1))))
+     `(org-level-2 ((t (,@headline ,@variable-tuple :height 1.15))))
+     `(org-level-1 ((t (,@headline ,@variable-tuple :height 1.25))))
+     `(org-document-title ((t (,@headline ,@variable-tuple :height 1.5 :underline nil))))))
 
   (custom-theme-set-faces
    'user
-   '(variable-pitch ((t (:family "Source Sans Pro" :height 180 :weight light))))
-   '(fixed-pitch ((t ( :family "Inconsolata" :slant normal :weight normal :height 1.0 :width normal)))))
+   '(variable-pitch ((t (:family "Source Sans Pro" :height 150 :weight light))))
+   '(fixed-pitch ((t ( :family "Source Sans Pro" :slant normal :weight normal :height 1.0 :width normal)))))
 
   ;; (add-hook 'org-mode-hook 'variable-pitch-mode)
   (add-hook 'org-mode-hook 'visual-line-mode)
@@ -87,6 +87,7 @@
    '(org-special-keyword       ((t (:inherit (font-lock-comment-face fixed-pitch)))))
    '(org-tag                   ((t (:inherit (shadow fixed-pitch) :weight bold :height 0.8))))
    '(org-verbatim              ((t (:inherit (shadow fixed-pitch))))))
+
   )
 
 (defun +org|setup-agenda ()
@@ -203,6 +204,11 @@
   (require 'evil-org-agenda)
   (evil-org-agenda-set-keys))
 
+(use-package mixed-pitch
+  :hook
+  ;; If you want it in all text modes:
+  (text-mode . mixed-pitch-mode))
+
 (use-package org-re-reveal
   :after org
   :config
@@ -258,21 +264,21 @@
   (add-hook 'org-tree-slide-mode-hook #'evil-normalize-keymaps)
 
   (general-define-key
-    :states '(normal visual)
-    :keymaps 'org-tree-slide-mode-map
-    "q" 'org-tree-slide-mode
-    "<up>" 'org-tree-slide-content
-    "<down>" 'org-tree-slide-display-header-toggle
-    "<left>" 'org-tree-slide-move-previous-tree
-    "<right>" 'org-tree-slide-move-next-tree)
+   :states '(normal visual)
+   :keymaps 'org-tree-slide-mode-map
+   "q" 'org-tree-slide-mode
+   "<up>" 'org-tree-slide-content
+   "<down>" 'org-tree-slide-display-header-toggle
+   "<left>" 'org-tree-slide-move-previous-tree
+   "<right>" 'org-tree-slide-move-next-tree)
   )
 
 (use-package htmlize
   :commands (htmlize-buffer
-              htmlize-file
-              htmlize-many-files
-              htmlize-many-files-dired
-              htmlize-region))
+             htmlize-file
+             htmlize-many-files
+             htmlize-many-files-dired
+             htmlize-region))
 
 (use-package plantuml-mode
   :defer t
