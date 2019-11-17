b554bd7ba192e5a877ea2bdee08eed5e07ef2cd1
Xiaoxing Hu <xiaoxing@huxx.org>
2019-11-17 22:12:50 +1300

    trying to get yasnippet into company

--- ~!.emacs.d/.lisp/feature-snippets.el
+++ ~!.emacs.d/.lisp/feature-snippets.el
@@ -1,3 +1,19 @@
+;; Add yasnippet support for all company backends
+;; https://github.com/syl20bnr/spacemacs/pull/179
+(defvar company-mode/enable-yas t
+  "Enable yasnippet for all backends.")
+
+(defun company-mode/backend-with-yas (backend)
+  (if (or (not company-mode/enable-yas) (and (listp backend) (member 'company-yasnippet backend)))
+      backend
+    (append (if (consp backend) backend (list backend))
+            '(:with company-yasnippet))))
+
+(defun x|enable-company-with-yas ()
+  (interactive)
+  (setq company-backends (mapcar #'company-mode/backend-with-yas company-backends))
+  )
+
 (use-package yasnippet
   :config
   (yas-global-mode 1))
