bc2bdf57f96cb73f097699eb48d587cccdede487
Xiaoxing Hu <xiaoxing@huxx.org>
2019-11-18 21:07:42 +1300

    treemacs

--- ~!.emacs.d/.lisp/feature-project.el
+++ ~!.emacs.d/.lisp/feature-project.el
@@ -31,6 +31,12 @@
     "p p" '(projectile-switch-project :which-key "Open Project")
     "p f" '(projectile-find-file :which-key "Find File")))
 
+(setq treemacs-follow-after-init t
+      treemacs-is-never-other-window t
+      treemacs-sorting 'alphabetic-case-insensitive-desc
+      treemacs-persist-file (expand-file-name "treemacs-persist" x/local-dir)
+      treemacs-last-error-persist-file (concat x/cache-dir "treemacs-last-error-persist"))
+
 ;;;###autoload
 (defalias 'doom-project-root #'projectile-project-root)
 
@@ -79,59 +85,10 @@ Use `treemacs' command for old functionality."
   :init
   (with-eval-after-load 'winum
     (define-key winum-keymap (kbd "M-0") #'treemacs-select-window))
-  :config
-  (progn
-    (setq treemacs-collapse-dirs              (if (executable-find "python") 3 0)
-          treemacs-deferred-git-apply-delay   0.5
-          treemacs-display-in-side-window     t
-          treemacs-file-event-delay           5000
-          treemacs-file-follow-delay          0.2
-          treemacs-follow-after-init          t
-          treemacs-follow-recenter-distance   0.1
-          treemacs-goto-tag-strategy          'refetch-index
-          treemacs-indentation                2
-          treemacs-indentation-string         " "
-          treemacs-is-never-other-window      nil
-          treemacs-no-png-images              nil
-          treemacs-project-follow-cleanup     nil
-          treemacs-persist-file               (expand-file-name "treemacs-persist" x/local-dir)
-          treemacs-recenter-after-file-follow nil
-          treemacs-recenter-after-tag-follow  nil
-          treemacs-show-hidden-files          t
-          treemacs-silent-filewatch           nil
-          treemacs-silent-refresh             nil
-          treemacs-sorting                    'alphabetic-desc
-          treemacs-space-between-root-nodes   t
-          treemacs-tag-follow-cleanup         t
-          treemacs-tag-follow-delay           1.5
-          treemacs-width                      35)
-
-    ;; The default width and height of the icons is 22 pixels. If you are
-    ;; using a Hi-DPI display, uncomment this to double the icon size.
-    (treemacs-resize-icons 16)
-
-    (treemacs-follow-mode t)
-    (treemacs-filewatch-mode t)
-    (treemacs-fringe-indicator-mode t)
-    (pcase (cons (not (null (executable-find "git")))
-                 (not (null (executable-find "python3"))))
-      (`(t . t)
-       (treemacs-git-mode 'extended))
-      (`(t . _)
-       (treemacs-git-mode 'simple))))
   :general
   (map!
     "t" '(:ignore t :which-key "Toggle")
     "t t" '(+treemacs/toggle :which-key "Treemacs"))
-
-  ;; :bind
-  ;; (:map global-map
-  ;;  ("M-0"       . treemacs-select-window)
-  ;;  ("C-x t 1"   . treemacs-delete-other-windows)
-  ;;  ("C-x t t"   . treemacs)
-  ;;  ("C-x t B"   . treemacs-bookmark)
-  ;;  ("C-x t C-t" . treemacs-find-file)
-  ;;  ("C-x t M-t" . treemacs-find-tag))
   )
 
 (use-package treemacs-evil
