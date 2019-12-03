bb399773075ea12abcd9890fd54e5022ff80292e
Xiaoxing Hu <xiaoxing@huxx.org>
2019-12-03 20:37:36 +1300

    switch emacs theme

--- ~!.emacs.d/.lisp/core-ui.el
+++ ~!.emacs.d/.lisp/core-ui.el
@@ -8,7 +8,7 @@
 
 (use-package doom-themes
   :config
-  (load-theme 'doom-dracula t)
+  (load-theme 'doom-vibrant t)
   ;; Enable custom neotree theme (all-the-icons must be installed!)
   ;;(doom-themes-neotree-config)
   ;; or for treemacs users
